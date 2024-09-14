3357574badc70ff0c1dfbb34c1a5e448657c3076
Andrei Makar-Uvarov <lyykfi@gmail.com>
2024-09-14 03:32:42 +0300

    wip

--- app/src/Controller/Task/TaskList.php
+++ app/src/Controller/Task/TaskList.php
@@ -5,23 +5,42 @@ namespace App\Controller\Task;
 require __DIR__ . '/../../../vendor/autoload.php';
 
 use App\Services\TaskService;
+use DateTime;
 
 class TaskList
 {
     private $taskService;
 
     public function __construct()
-    {
+    {   
         $this->taskService = new TaskService();
     }
+
+    private function seeds(): void {
+        session_start();
+
+        $lastSeeded = $_SESSION["task_seeded"];
+        $date = new DateTime();
+
+        if (!$lastSeeded || ($lastSeeded && $date->diff(new DateTime('@'    .$lastSeeded))->h > 1)) {
+            $this->taskService->seedTasks();
+
+            $_SESSION["task_seeded"] = $date->getTimestamp(); 
+        }
+    }
+
     public function __invoke(): void
     {
+        $this->seeds();
+        
         $tasks = $this->taskService->getAllTasks();
 
         header('Content-Type: application/json;charset=utf-8');  
         echo json_encode($tasks, JSON_UNESCAPED_UNICODE);
     }
 }
+
+
 
 $controller = new TaskList();
 
--- app/src/Services/TaskService.php
+++ app/src/Services/TaskService.php
@@ -3,6 +3,7 @@
 namespace App\Services;
 
 use App;
+use DateTime;
 
 class TaskService {
     private $db;
@@ -14,16 +15,38 @@ class TaskService {
         $this->pdo = $this->db->getPDO();
     }
 
-    public function getAllTasks() {
+    public function getAllTasks(): array {
         $stmt = $this->pdo->query("SELECT * FROM task");
 
         return $stmt->fetchAll(\PDO::FETCH_ASSOC);
     }
 
-    public function getTaskById(int $id) {
+    public function getTaskById(int $id): mixed {
         $stmt = $this->pdo->prepare("SELECT * FROM task WHERE id=:id");
         $stmt->execute(['id' => $id]);
 
         return $stmt->fetch(\PDO::FETCH_ASSOC);
+    }
+
+    public function seedTasks(): void {
+        $sql = "INSERT INTO `task` (`title`, `data`, `author`, `status`, `description`) VALUES (:title, :data, :author, :status, :description)";
+        $date = new DateTime();
+
+        for($i = 1; $i <= 1000; $i++) {
+            if ($i != 0) {
+                $date->add(new \DateInterval('PT1H'));
+            }
+
+            $data = [
+                'title' => "Задача $i",
+                'data' => date("Y-m-d H:i:s", $date->getTimestamp()),
+                'author' => "Автор $i",
+                'status' => "Статус $i",
+                'description' => "Описание $i",
+            ];
+
+            $this->pdo->prepare($sql)->execute($data);
+
+        }
     }
 }
